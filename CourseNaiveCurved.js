94dadc8ff537f75114aa13bb4a8106c2519b3c86
Paul van Bemmelen <pvbemmelen62@gmail.com>
2021-05-24 18:53:45 +0200

    Removed some comment lines.

--- CourseNaiveCurved.js
+++ CourseNaiveCurved.js
@@ -40,30 +40,6 @@ CourseNaiveCurved.prototype = {
       ctx.fillStyle = "red";
       ctx.fill();
     }
-
-//  var lineIter = new Linear(ll0.lon,_that.f(ll0.lat),ll1.lon,_that.f(ll1.lat)).iterator(10);
-//  ctx.beginPath();
-//  var lfl = lineIter.next().value;
-
-//  var ll = _that.fInv(lfl[0],lfl[1]);
-//  var cv = lfl2cv.map(ll[0], _this.f(ll[1]));
-//  ctx.moveTo(cv[0], cv[1]);
-//  for(var lfl of lineIter) {
-//    ll = _that.fInv(lfl[0],lfl[1]);
-//    cv = lfl2cv.map(ll[0], _this.f(ll[1]));
-//    ctx.lineTo(cv[0], cv[1]);
-//  }
-//  ctx.strokeStyle = "#000000";
-//  ctx.stroke();
-
-//  var cv0 = lfl2cv.map(ll0.lon, _this.f(ll0.lat));
-//  var cv1 = lfl2cv.map(ll1.lon, _this.f(ll1.lat));
-//  for(var cv of [cv0,cv1]) {
-//    ctx.beginPath();
-//    ctx.arc(cv[0], cv[1], arcRadius, 0, 2 * Math.PI, false);
-//    ctx.fillStyle = "red";
-//    ctx.fill();
-//  }
   },
   move : function(point, ll) {
     if("ll0" == point) {
